ef77f6b89403cbec7db9e35fef6791ea0421340d
maulllanamm <>
2022-11-29 09:29:14 +0700

    fix icon

---
+++ src/Components/Atoms/Icons.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaReact } from '@react-icons/all-files/fa/FaReact';
+import { FaNodeJs } from '@react-icons/all-files/fa/FaNodeJs.esm';
+import { DiPhp } from '@react-icons/all-files/di/DiPhp';
+
+export default function Icons({ icons }) {
+  if (icons === 'react') {
+    return (
+      <div className="icons">
+        <FaReact size={40} color="#38bdf8" />
+      </div>
+    );
+  } else if (icons === 'node') {
+    return (
+      <div className="icons">
+        <FaNodeJs size={40} color="#89BB3C" />;
+      </div>
+    );
+  } else {
+    return (
+      <div className="icons">
+        <DiPhp size={40} color="#4B568C" />;
+      </div>
+    );
+  }
+}
--- src/Components/Molecules/ProfilePicture.js
+++ src/Components/Molecules/ProfilePicture.js
@@ -1,31 +1,25 @@
 import React from 'react';
-import Blob1 from '../../Assets/Image/blob1.svg';
-import Blob2 from '../../Assets/Image/blob2.svg';
 import ProfilePic from '../../Assets/Image/pas-foto.png';
-import NodeIcon from '../Atoms/NodeIcon';
-import PhpIcon from '../Atoms/PhpIcon';
-import ReactIcon from '../Atoms/ReactIcon';
+import Icons from '../Atoms/Icons';
 import './Molecules.css';
 
 export default function ProfilePicture() {
   return (
-    <>
-      <div className="profile-icon">
-        <div className="react-icon">
-          <ReactIcon />
-        </div>
-        <div className="node-icon">
-          <NodeIcon />
-        </div>
-        <div className="php-icon">
-          <PhpIcon />
-        </div>
-      </div>
+    <div className="profile-picture-container">
       <div className="profile-picture">
-        {/* <img src={Blob1} alt="blob1" />
-      <img src={Blob2} alt="blob2" /> */}
+        <div className="profile-icon">
+          <div className="react-icon">
+            <Icons icons="react" />
+          </div>
+          <div className="node-icon">
+            <Icons icons="node" />
+          </div>
+          <div className="php-icon">
+            <Icons icons="php" />
+          </div>
+        </div>
         <img src={ProfilePic} alt="profile-pic" />
       </div>
-    </>
+    </div>
   );
 }
